2abb4565be501ed51b42ad13ca3985bad99023fb
prajith17 <prajithkrish17@gmail.com>
2023-08-23 12:57:49 +0530

    challenge

---
+++ src/Org/archa/Arraychal.java
@@ -0,0 +1,35 @@
+package Org.archa;
+
+public class Arraychal {
+	public static void main(String[] args) {
+		private void m1(){
+			m2();
+			System.out.println(1)
+			}
+			private void m2(){
+			m3();
+			System.out.print2);
+			}
+			private void m3(){
+			System.out.println();
+			try{
+			int sum = 4/0;
+			System.out.printf(“4”);
+			}
+			catch(ArithmeticException e){
+			System.out.printf(“5”);
+			}
+			System.out.printf(“7”);
+			}
+			public static void main(String[] args){
+			Test obj = new Test();
+			obj.m1();
+			}
+			}
+			
+			
+			 }
+			 }
+		
+	}
+
