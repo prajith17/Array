dd3541d97578ac9eba3e4d2515aff5b5490c344d
prajith17 <prajithkrish17@gmail.com>
2023-08-23 14:34:41 +0530

    java added

---
+++ src/Org/archa/Bmw.java
@@ -0,0 +1,11 @@
+package Org.archa;
+
+public class Bmw {
+public void mileage(String mil) {
+	System.out.println("mileage is " + mil);
+}
+public void enginepower(String ep) {
+	
+System.out.println("engine power is " + ep);
+}
+}
